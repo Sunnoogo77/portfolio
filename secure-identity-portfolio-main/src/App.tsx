9997a4664c4d61315bb9e5ba0037dbbeea19844e
Sunnoogo77 <sunnoogo-caleb.tapsoba@efrei.net>
2026-01-29 10:24:08 +0100

    Update App.tsx

--- secure-identity-portfolio-main/src/App.tsx
+++ secure-identity-portfolio-main/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+const baseName = import.meta.env.PROD ? "/portfolio" : "/";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -13,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={baseName}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
